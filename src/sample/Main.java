7b87bd05484ee2d497f54e767b42a0cb268558fc
Myeongkeun Kim <grove156@gmail.com>
2021-07-15 19:56:09 +0900

    settigns

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,9 +14,4 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
-
-
-    public static void main(String[] args) {
-        launch(args);
-    }
 }
